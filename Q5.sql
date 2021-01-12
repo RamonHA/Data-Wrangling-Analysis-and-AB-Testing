52bf9599eaa4542587aae9664922cd30ef95ea44
RamonHA <33186300+RamonHA@users.noreply.github.com>
2021-01-11 18:54:25 -0600

    Create Q5.sql

---
+++ Q5.sql
@@ -0,0 +1,13 @@
+--Use the https://thumbtack.github.io/abba/demo/abba.html to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence. 
+
+-- For the views_binary:
+-- The p-value is on 0.36 meaning that the results are not statistically significant
+-- The calcultaed lift is 1.9%
+-- And the range of improvement is of -2.1% to 5.9%
+-- Being the p-value above a 10% level of significance, 
+-- shows a no significance difference in the test between the control and test group.
+
+-- For the order binary:
+-- The p-value is of 0.74, with and improvement range of -15% to 11%.
+-- This one too, its p-value is above a 10% significance level
+-- so no significance difference is found between the control and test group.
